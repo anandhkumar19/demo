6aa6fc021995b8e20968a09f6ab1d0de2e386314
RabidKumar <rabid.das@gmail.com>
2017-07-13 11:59:07 +0530

    added sysouts

--- SeleniumLearning/src/main/java/Pratice/Addtion.java
+++ SeleniumLearning/src/main/java/Pratice/Addtion.java
@@ -3,9 +3,11 @@ package Pratice;
 public class Addtion {
 public static void main(String[] args) {
 	int add=0;
+System.out.println("Addition of integers upto 10");
 	for(int i=0;i<=10;i++){
 		add=add+i;
 		}
+System.out.println(add);
 	
 }
 }
